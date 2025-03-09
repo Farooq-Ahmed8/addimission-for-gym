6535257d1eb39426c783bf33da1a926dad5835fe
Farooq Ahmed <03174748082ali@gmail.com>
2025-03-09 18:31:27 +0500

    my third commit

--- app.js
+++ app.js
@@ -2,4 +2,12 @@
 
 var userName =prompt('Enter Your Name')
 var age = +prompt('Enter Your Age')
-document.write('Name: ' + userName +'</br>')
+document.write('<h1>' + 'Name: ' + userName + '</h1>' +'</br>')
+
+if (age >= 18){
+    document.write('<h1>' + 'In ' + age + ' Years you can Join the gum ' + '</h1>')
+}else if(age <= 18){
+    document.write('<h1>' + 'Your are not ready for gym ' + '</h1>')
+}else{
+    document.write('<h1>' + 'Not Allowed ' + '</h1>')
+}
